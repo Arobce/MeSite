f6b7c6e1c94fff6da4c8212ca5616d16f91d49f3
Roshan Chapagain <arobce@gmail.com>
2018-10-28 13:35:25 +0545

    Initial Messge

---
+++ index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Roshan Chapagain | Full Stack Web Developer & Designer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <!-- Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128237410-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-128237410-1');
+    </script>    
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <!-- Bulma -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-tooltip@2.0.2/dist/css/bulma-tooltip.min.css">
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
+    <!-- Script -->
+    <script src="js/main.js"></script>
+</head>
+
+<body>
+    <!-- Head Section -->
+    <header id="intro " class="section">
+        <h2 class="title is-1"><span>Hi, I am </span><a href="http://bit.ly/2D6KG0q" class="tooltip is-tooltip-info" data-tooltip="View my CV">Roshan Chapagain</a></h2>
+        <p>
+            I am a <span class="red">passionate</span> Full Stack Web Developer. <br>
+            I make <span class="green">usable</span>, <span class="blue">reliable</span> and <span class="purple">efficient</span> web solutions. <br>
+            I am also an avid reader.
+        </p>
+    </header>
+    <!-- What I do Section -->
+    <section id="services" class="section">
+
+        <h2 class="title">What I do?</h2>
+        <div class="block">
+            <div class="columns">
+                <div class="services-design column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child box">
+                            <img src="assets/design.png">
+                            <p class="title">Design</p>
+                            <div class="content">
+                                <p class="description">I value simple clean layouts that provide superior User Experience. </p>
+                                <div class="things-like">
+                                    <p class="things-title">Things I enjoy designing:</p>
+                                    <p class="things">UX, UI, Web, Mobile, Apps, Logos</p>
+                                </div>
+                                <div class="tools-used">
+                                    <p class="tools-title">Design Tools:</p>
+                                    <div class="tools">
+                                        <ul>
+                                            <li>Adobe Photoshop</li>
+                                            <li>Adobe Illustrator</li>
+                                            <li>Adobe XD</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div> <!-- content -->
+                        </article>
+                    </div><!-- tile -->
+                </div> <!-- services-design -->
+                <div class="services-web column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child box">
+                            <img src="assets/code.png">
+                            <p class="title">Web Application Development</p>
+
+                            <div class="content">
+                                <p class="description">I build reliable, responsive and efficient
+                                    web solutions using the most current technologies
+                                    to provide the best user experience while
+                                    satisfying customer needs. I also have some experience building PWAs. </p>
+                                <div class="things-like">
+                                    <p class="things-title">Languages I speak:</p>
+                                    <p class="things">HTML, CSS, Javascript, Sass, PHP</p>
+                                </div>
+                                <div class="tools-used">
+                                    <p class="tools-title">Dev Tools:</p>
+                                    <div class="tools">
+                                        <ul>
+                                            <li>Visual Studio Code</li>
+                                            <li>Github</li>
+                                            <li>Vue.js</li>
+                                            <li>Wordpress</li>
+                                            <li>Laravel</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div> <!-- content -->
+                        </article>
+                    </div><!-- tile -->
+                </div> <!-- services-web -->
+                <div class="services-mobile column">
+                    <div class="tile is-parent">
+                        <article class="tile is-child box">
+                            <img src="assets/phone.png">
+                            <p class="title">Mobile Application Development</p>
+                            <div class="content">
+                                <p class="description">I build Android, as well as hybrid pplications that both look and feel good
+                                    with minimum possible app size, applying best practices. </p>
+                                <div class="things-like">
+                                    <p class="things-title">Languages I speak:</p>
+                                    <p class="things">Java, Javascript</p>
+                                </div>
+                                <div class="tools-used">
+                                    <p class="tools-title">Dev Tools:</p>
+                                    <div class="tools">
+                                        <ul>
+                                            <li>Android Studio</li>
+                                            <li>Onsen UI</li>
+                                            <li>React Native</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div> <!-- content -->
+                        </article>
+                    </div><!-- tile -->
+                </div> <!-- services-mobile -->
+            </div> <!-- columns-->
+        </div> <!-- block -->
+    </section>
+    <!-- Portfolio Section -->
+    <section id="portfolio" class="section">
+
+        <h2 class="title">My Works</h2>
+        <h4 class="subtitle">View some of my Works.</h4>
+        <div class="block">
+            <div class="columns">
+                <div class="column">
+                    <figure class="image is-3by2">
+                        <img class="project-thumb" src="assets/mausec.jpg">
+                        <figcaption>
+                            <h1 class="title is-size-5 is-size-4-widescreen">An NGO actively working in the sectors of health, education, and equality. </h1>
+                            <a class="button is-primary is-inverted is-outlined" disabled href="http://traversity.herokuapp.com/">
+                                <span>Under Construction</span>
+                                <span class="icon is-small">
+                                    <i class="fas fa-hammer"></i>
+                                </span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="column">
+                    <figure class="image is-3by2">
+                        <img class="project-thumb" src="assets/traversity.jpg">
+                        <figcaption>
+                            <h1 class="title is-size-5 is-size-4-widescreen">Hackathon winning, intelligent and flexible travel companion.</h1>
+                            <a class="button is-primary is-inverted is-outlined" href="http://traversity.herokuapp.com/">
+                                <span>Visit Website</span>
+                                <span class="icon is-small">
+                                    <i class="fas fa-angle-right"></i>
+                                </span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="column">
+                    <figure class="image is-3by2">
+                        <img class="project-thumb" src="assets/people.jpg">
+                        <figcaption>
+                            <h1 class="title is-size-5 is-size-4-widescreen">Inspiring new social network.</h1>
+                            <a class="button is-primary is-inverted is-outlined" disabled href="http://traversity.herokuapp.com/">
+                                <span>Under Construction</span>
+                                <span class="icon is-small">
+                                    <i class="fas fa-hammer"></i>
+                                </span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div><!-- columns -->
+        </div> <!-- block -->
+
+    </section>
+    <!-- Testimonials Section -->
+    <section id="testimonials" class="section">
+
+        <h2 class="title">Testimonials</h2>
+        <h4 class="subtitle">What my clients and co-workers say about me.</h4>
+        <div class="columns">
+            <div class="first column">
+                <div class="testimonial">
+                    <p class="text">Roshan has been a delight to work with. He
+                        is proactive, self-motivated and driven. </p>
+                </div>
+                <img class="avatar" src="assets/bibhuti.jpeg">
+                <p class="name"><b>Bibhuti Poudyal</b></p>
+                <p class="position">Software Engineer</p>
+            </div><!-- first -->
+            <div class="second column">
+                <div class="testimonial">
+                    <p class="text">Roshan, not only built a great system for our organization but also
+                        gave us ways to reach a wider public allowing us to serve better. Will definately recommend him.
+                    </p>
+                </div>
+                <img class="avatar" src="assets/sunil.jpg">
+                <p class="name"><b>Sunil Kumar</b></p>
+                <p class="position">Administrative Manager, MAUSEC-Nepal</p>
+            </div>
+            <div class="bubble"></div><!-- second -->
+            <div class="third column">
+                <div class="testimonial">
+                    <p class="text">A pleasent guy to work with.</p>
+                </div>
+                <img class="avatar" src="assets/sagar.jpg">
+                <p class="name"><b>Sagar Dhungel</b></p>
+                <p class="position">Data Scientist</p>
+            </div><!-- third -->
+        </div><!-- columns -->
+    </section>
+
+    <!-- ContactMe Section -->
+    <section id="contact" class="section">
+        <h2 class="title">Work with me</h2>
+        <h4 class="subtitle">Lets build something together!</h4>
+        <div class="form columns">
+            <form name="contact-form" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <span class="field-label">Name</span><br>
+                <div class="field">
+                    <p class="control has-icons-left">
+                        <input class="input" type="text" name="name" placeholder="Name">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
+                    </p>
+                </div>
+                <span class="field-label">Phone</span><br>
+                <div class="field">
+                    <p class="control has-icons-left">
+                        <input class="input" type="number" name="phone" placeholder="Phone">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-phone"></i>
+                        </span>
+                    </p>
+                </div>
+                <span class="field-label">Email</span><br>
+                <div class="field">
+                    <p class="control has-icons-left">
+                        <input class="input" type="email" placeholder="Email" name="email" required>
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                    </p>
+                </div>
+                <span class="field-label">Message</span><br>
+                <div class="field">
+                    <p class="control has-icons-left">
+                        <textarea rows="5" cols="50" class="input is-large" name="message" placeholder="Message" required></textarea>
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-comment"></i>
+                        </span>
+                    </p>
+                </div>
+                <input type="submit" class="button is-fullwidth" name="submit" value="SEND">
+            </form>
+
+        </div>
+    </section>
+    <!-- Send Email -->
+    <?php 
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $to = "arobce@gmail.com";
+        $headers = "To:".$to. "\r\n" ."From:".$email. "\r\n" ;
+        // To send HTML mail, the Content-type header must be set
+        $headers  .= 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+        //Final Message
+        $finalMessage = "Name: ".$name."<br>"."Phone: ".$phone."<br>"."Email: ".$email."<br>"."Message: "
+                            .$message."<br>";
+        
+        // Send Email
+        mail($to,"From Website",$finalMessage,$headers);
+
+    }
+    ?>
+    <!-- Footer Section -->
+    <footer>
+        <div class="logo-wrapper">
+            <img src="assets/logo.png" alt="RC">
+        </div>
+
+        <p class="slogan"><span class="purple">Living</span> ,<span class="blue">Learning</span>
+            and <span class="green">Growing</span> everyday.</p>
+
+        <div class="social block">
+            <div class="columns is-gapless">
+                <div class="column">
+                    <a href="https://www.goodreads.com/user/show/35002807-roshan-chapagain">
+                        <span class="icon is-large">
+                            <i class="fa-2x fab fa-goodreads-g"></i>
+                        </span>
+                    </a>
+                </div>
+                <div class="column">
+                    <a href="https://www.quora.com/profile/Roshan-Chapagain-1">
+                        <span class="icon is-large">
+                            <i class="fa-2x fab fa-quora"></i>
+                        </span>
+                    </a>
+                </div>
+                <div class="column">
+                    <a href="https://www.linkedin.com/in/roshan-chapagain-015112112/">
+                        <span class="icon is-large">
+                            <i class="fa-2x fab fa-linkedin"></i>
+                        </span>
+                    </a>
+                </div>
+            </div> <!-- columns -->
+        </div> <!-- social block-->
+        <p class="copyright">handcrafted by me &copy; <span class="blue">twenty</span><span class="purple">eighteen</span></p>
+    </footer>
+</body>
+
+</html>
